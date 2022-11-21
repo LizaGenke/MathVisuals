7135f38f32ed98eb263ac5253c94d2ce4fc10678
LizaGenke <lizagenke@gmail.com>
2022-11-21 15:41:16 +0100

    support degenerate stars

--- stars.js
+++ stars.js
@@ -23,43 +23,67 @@ stepSlider.oninput = function() {
     newStarPlot(sizeSlider.value, this.value);
 }
 
-function getYCoords(starSize, starStep)
+function getYCoords(starSize, starStep, angle)
 {
     output_y_coords = []
     for (let i = 0; i < (starSize + 1); i++)
     {
-        angle_rad = i*starStep*2*Math.PI/starSize;
+        angle_rad = angle + i*starStep*2*Math.PI/starSize;
         output_y_coords.push(Math.cos(angle_rad));
     }
     return output_y_coords;
 }
 
-function getXCoords(starSize, starStep)
+function getXCoords(starSize, starStep, angle)
 {
     output_x_coords = []
     for (let i = 0; i < (starSize + 1); i++)
     {
-        angle_rad = i*starStep*2*Math.PI/starSize;
+        angle_rad = angle + i*starStep*2*Math.PI/starSize;
         output_x_coords.push(Math.sin(angle_rad));
     }
     return output_x_coords;
 }
 
 function makeTrace(starSize, starStep) {
-    return {
-        y: getYCoords(starSize, starStep),
-        x: getXCoords(starSize, starStep),
-        line: {
-            color: 'black',
-            width: 0.5
-        },
-        visible: true,
-        name: 'Star',
-    };
+    if (starSize%starStep != 0 || starStep==1)
+    {
+        return [{
+            y: getYCoords(starSize, starStep, 0),
+            x: getXCoords(starSize, starStep, 0),
+            line: {
+                color: 'black',
+                width: 0.5
+            },
+            visible: true,
+            name: 'Star',
+        }];
+    }
+    else
+    {
+        data = [];
+        number_of_polygons = starStep;
+        for (let i = 0; i < number_of_polygons; i++)
+        {
+            start_angle = i*2*Math.PI/starSize;
+            var trace = {
+                y: getYCoords(starSize, starStep, start_angle),
+                x: getXCoords(starSize, starStep, start_angle),
+                line: {
+                    color: 'blue',
+                    width: 0.5
+                },
+                visible: true,
+                showlegend: false,
+            };
+            data.push(trace);
+        }
+        return data;
+    }
 }
 
 function newStarPlot(starSize, starStep) {
-    Plotly.newPlot('graph', Array(makeTrace(starSize, starStep)), {
+    Plotly.newPlot('graph', makeTrace(starSize, starStep), {
         xaxis: {
             range: [-1.0, 1.0],
         },
